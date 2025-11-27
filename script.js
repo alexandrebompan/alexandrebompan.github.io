e3351eafb1664584187733e8ad74b40af1963c74
Alexandre Bompan <alexandre.bompan@edu.unifil.br>
2025-11-27 04:08:52 -0300

    Atualização para uma versão melhorada do portfólio

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// Smooth scroll dos links do menu
-document.querySelectorAll("nav a").forEach(link => {
-    link.addEventListener("click", e => {
-        e.preventDefault();
-        const target = document.querySelector(link.getAttribute("href"));
-        window.scrollTo({
-            top: target.offsetTop - 70,
-            behavior: "smooth"
-        });
-    });
-});
+console.log('Loaded');
